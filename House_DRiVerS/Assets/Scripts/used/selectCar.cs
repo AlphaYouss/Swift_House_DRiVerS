a8bf4f00b8a85f9574c4dd80618c8e64de525858
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-12-18 02:25:07 +0100

    Version 0.5.3
    +- Updated Singleplayer generaltrack
    +- Updated practice generaltrack
    + Added new folders (UI, Images and Fonts)
    + UI (In progress)
    +- Added UI scripts

---
+++ House_DRiVerS/Assets/Scripts/used/selectCar.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class selectCar : MonoBehaviour {
+
+    public RawImage image;
+    public Button ravensButton;
+    public Button dragonsButton;
+    public Button serpentsButton;
+    public Button vikingsButton;
+
+    public Button back;
+
+	// Use this for initialization
+	void Start () {
+        ravensButton = ravensButton.GetComponent<Button>();
+        dragonsButton = dragonsButton.GetComponent<Button>();
+        serpentsButton = serpentsButton.GetComponent<Button>();
+        vikingsButton = vikingsButton.GetComponent<Button>();
+
+        back = back.GetComponent<Button>();
+    }
+
+    public void SelectCar()
+    {
+
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+}
