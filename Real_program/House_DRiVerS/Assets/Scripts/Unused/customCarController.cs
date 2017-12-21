9fc9e62e48691bb77735a0cc669801ea551a997b
Youssef el Jaddaoui <d18322@edu.rocwb.nl>
2017-12-21 02:16:52 +0100

    Version 0.6.2
    * Car particals are coming in the next versions
    
    +- Updated in-game folders
    + Added timer in the singleplayerstandard track
    + Added switching camera's with a keyboard
    + Added Xbox controls
    + Added custom car controls (unfinished) with physics
    + Build game for testing purposes

---
+++ Real_program/House_DRiVerS/Assets/Scripts/Unused/customCarController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Car;
+using System.Collections.Generic;
+using System;
+
+public class customCarController : MonoBehaviour
+{
+    [SerializeField] private WheelCollider[] m_WheelColliders = new WheelCollider[4];
+    [SerializeField] private GameObject[] m_WheelMeshes = new GameObject[4];
+    [SerializeField] private WheelEffects[] m_WheelEffects = new WheelEffects[4];
+    [SerializeField] private Quaternion[] m_WheelMeshLocalRotations;
+
+	public static Rigidbody carRigidbody;
+
+    float velocity;
+    float maxSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        velocity = 1;
+        maxSpeed = 1000;
+    }
+
+    private void Awake()
+    {
+        carRigidbody = GetComponent<Rigidbody>();
+        m_WheelMeshLocalRotations = new Quaternion[4];
+        //for (int i = 0; i < 4; i++)
+        //{
+        //    m_WheelMeshLocalRotations[i] = m_WheelMeshes[i].transform.localRotation;
+        //}
+        carRigidbody.centerOfMass = new Vector3(carRigidbody.centerOfMass.x, -1000f, carRigidbody.centerOfMass.z);
+    }
+
+    //private bool OnCollision()
+    //{
+    //    if (Physics.Raycast(origin: transform.position, direction: Vector3.forward, maxDistance: 1.5f)) {
+    //        velocity = (velocity / 10) * -1;
+    //        if(velocity >= 0)
+    //        {
+    //            velocity = -1;
+    //        }
+    //        transform.Translate(new Vector4(0, 0, velocity / 1000));
+    //        return true;
+    //    }
+    //    if(Physics.Raycast(transform.position,Vector3.back,1f)) {
+    //        velocity = (velocity / 10) * -1;
+    //        if (velocity <= 0)
+    //        {
+    //            velocity = 1;
+    //        }
+    //    }
+    //    if (Physics.Raycast(origin: transform.position, direction: Vector3.left, maxDistance: 1f))
+    //    {
+    //        transform.Translate(0.5f, 0, 0);
+    //    }
+    //    if (Physics.Raycast(origin: transform.position, direction: Vector3.right, maxDistance: 1f))
+    //    {
+    //        transform.Translate(-0.5f, 0, 0);
+    //    }
+    //    return false;
+    //}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 150, 40), "velocity = " + velocity);
+    }
+		
+    //Move the car with the right speed and rotation
+	public static void MoveCar(float rotation, float velocity)
+    {
+		carRigidbody.transform.Translate(new Vector4(0, 0, velocity / 1000));
+		carRigidbody.transform.Rotate (0, rotation, 0);
+        //carRigidbody.AddForce(new Vector3(0, 0, velocity), ForceMode.Impulse);
+
+        //carRigidbody.AddForce(new Vector3(0, -10f, 0));
+        //so far this is the best option only problem is that it likes to go flying
+    }
+}
---
+++ Testing/Test_House_DRiVerS/Assets/Scripts/Used/LoadLapTime.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadLapTime : MonoBehaviour
+{
+
+    public int MinCount;
+    public int SecCount;
+    public float MilliCount;
+
+    public int MinCount2;
+    public int SecCount2;
+    public float MilliCount2;
+
+    public int MinCount3;
+    public int SecCount3;
+    public float MilliCount3;
+
+    public GameObject MinDisplay;
+    public GameObject SecDisplay;
+    public GameObject MilliDisplay;
+
+    public GameObject MinDisplay1;
+    public GameObject SecDisplay1;
+    public GameObject MilliDisplay1;
+
+    public GameObject MinDisplay2;
+    public GameObject SecDisplay2;
+    public GameObject MilliDisplay2;
+
+    void Start()
+    {
+        MinCount = PlayerPrefs.GetInt("MinSave");
+        SecCount = PlayerPrefs.GetInt("SecSave");
+        MilliCount = PlayerPrefs.GetFloat("MilliSave");
+
+        MinCount2 = PlayerPrefs.GetInt("MinSave2");
+        SecCount2 = PlayerPrefs.GetInt("SecSave2");
+        MilliCount2 = PlayerPrefs.GetFloat("MilliSave2");
+
+        MinCount3 = PlayerPrefs.GetInt("MinSave3");
+        SecCount3 = PlayerPrefs.GetInt("SecSave3");
+        MilliCount3 = PlayerPrefs.GetFloat("MilliSave3");
+
+        MinDisplay.GetComponent<Text>().text = "" + MinCount + ":";
+        SecDisplay.GetComponent<Text>().text = "" + SecCount + ".";
+        MilliDisplay.GetComponent<Text>().text = "" + MilliCount;
+
+        MinDisplay1.GetComponent<Text>().text = "" + MinCount2 + ":";
+        SecDisplay1.GetComponent<Text>().text = "" + SecCount2 + ".";
+        MilliDisplay1.GetComponent<Text>().text = "" + MilliCount2;
+
+        MinDisplay2.GetComponent<Text>().text = "" + MinCount3 + ":";
+        SecDisplay2.GetComponent<Text>().text = "" + SecCount3 + ".";
+        MilliDisplay2.GetComponent<Text>().text = "" + MilliCount3;
+
+    }
+}
+
+
